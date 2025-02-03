498ada0ab0528b050e0d222e041730f940871b8e
PFBregon <bregon@gmail.com>
2025-02-03 22:10:45 +0100

    first test with code goes wrong

--- Desktop/loops/src/test/java/dev/patriciafb/loops/LoopTest.java
+++ Desktop/loops/src/test/java/dev/patriciafb/loops/LoopTest.java
@@ -1,5 +1,14 @@
 package dev.patriciafb.loops;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
 public class LoopTest {
 
+    public void TestMultiplicacion() {
+        int n = 5;
+        List<String> result = Loop.Multiplicar(n);
+        assertEquals(10, result.size());
+    }
 }
